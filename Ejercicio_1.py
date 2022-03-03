6ecb9dce08a0add19f331cb936038c9064603564
Luis Torres <ltorres49@cuc.edu.co>
2022-03-02 19:40:11 -0500

    Ejercicio 1

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -5,3 +5,15 @@ Editor de Spyder
 Este es un archivo temporal.
 """
 
+donacion = float(input('Ingrese la donacion \n'))
+
+
+administracion = donacion*0.35
+sistemas = administracion*0.25
+telecomunicaciones = sistemas*0.1
+contabilidad = donacion - (telecomunicaciones + sistemas + administracion)
+
+print('El valor que le corresponde a telecomunicaciones es:', telecomunicaciones)
+print('El valor que le corresponde a sistemas es:', sistemas)
+print('El valor que le corresponde a administracion es:', administracion)
+print('El valor que le corresponde a contabilidad es:', contabilidad)
